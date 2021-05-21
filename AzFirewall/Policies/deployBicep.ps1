f6cdbf4bfef1c119a3daa7adabe359b828a64f38
Robert Lightner <rolightn@microsoft.com>
2021-05-21 10:18:05 -0700

    Updated interaction so user can enter RG Name

--- AzFirewall/Policies/deployBicep.ps1
+++ AzFirewall/Policies/deployBicep.ps1
@@ -1,9 +1,12 @@
+Get-AzSubscription | select name
 $subscriptionName = Read-Host -Prompt "Enter Subscription Name"
 Select-AzSubscription -SubscriptionName $subscriptionName
+Get-AzResourceGroup | select ResourceGroupName
+$resourceGroup = Read-Host -Prompt "Enter the Resource Group Name (where ARM template will be deployed into)"
 $bicepFile = '.\main.bicep'
 $mainParametersFiles = '.\main.parameters.json'
 New-AzResourceGroupDeployment `
   -Name coreFWPolicy `
-  -ResourceGroupName demo `
+  -ResourceGroupName $resourceGroup `
   -TemplateFile $bicepFile `
   -TemplateParameterFile $mainParametersFiles
