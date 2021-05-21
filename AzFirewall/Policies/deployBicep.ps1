53d332a39b9681be5af8f591fb3a17df8cc899cd
Robert Lightner <rolightn@microsoft.com>
2021-05-21 11:25:05 -0700

    initial commit

--- AzFirewall/Policies/deployBicep.ps1
+++ AzFirewall/Policies/deployBicep.ps1
@@ -1,7 +1,15 @@
-Get-AzSubscription | select name
+# Determine if already connected to Azure
+try {
+  $connected = Get-AzSubscription
+}
+catch {
+  Write-Host "Not connected and will prompt you to connect to Azure"
+}
+
+(Get-AzSubscription).name
 $subscriptionName = Read-Host -Prompt "Enter Subscription Name"
 Select-AzSubscription -SubscriptionName $subscriptionName
-Get-AzResourceGroup | select ResourceGroupName
+(Get-AzResourceGroup).ResourceGroupName
 $resourceGroup = Read-Host -Prompt "Enter the Resource Group Name (where ARM template will be deployed into)"
 $bicepFile = '.\main.bicep'
 $mainParametersFiles = '.\main.parameters.json'
