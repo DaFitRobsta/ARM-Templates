7e8a10f8c88be1dc40245b5667e45fae46bfc60f
Robert Lightner <rolightn@microsoft.com>
2021-05-21 17:28:43 -0700

    Friday checkin

--- AzFirewall/Policies/deployBicep.ps1
+++ AzFirewall/Policies/deployBicep.ps1
@@ -3,13 +3,19 @@ try {
   $connected = Get-AzSubscription
 }
 catch {
-  Write-Host "Not connected and will prompt you to connect to Azure"
+  Write-Host "Not connected to Azure and you will prompt you to connect to Azure" -ForegroundColor Green
+  $result = Connect-AzAccount
 }
-
+Write-Host ""
+Write-Host "List of available subscriptions:" -ForegroundColor Green
 (Get-AzSubscription).name
+Write-Host ""
 $subscriptionName = Read-Host -Prompt "Enter Subscription Name"
-Select-AzSubscription -SubscriptionName $subscriptionName
+$result = Select-AzSubscription -SubscriptionName $subscriptionName
+Write-Host ""
+Write-Host "List of available Resrouce Groups:" -ForegroundColor Green
 (Get-AzResourceGroup).ResourceGroupName
+Write-Host ""
 $resourceGroup = Read-Host -Prompt "Enter the Resource Group Name (where ARM template will be deployed into)"
 $bicepFile = '.\main.bicep'
 $mainParametersFiles = '.\main.parameters.json'
